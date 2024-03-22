78486549d571dcf967af8bdeeb89c463ab0dcbaf
username <maks.guz.98@bk.ru>
2024-03-22 15:41:37 +0300

    HW-4: Переделать методы findLeapYearNames, findOlderAnimal, findDuplicate под работы для StreamApi. Создать методы findAverageAge, findOldAndExpensive, findMinConstAnimals.

--- HW-1/src/main/java/Main.java
+++ HW-1/src/main/java/Main.java
@@ -1,22 +1,4 @@
-import animal.AbstractAnimal;
-import animal.Animal;
-import animal.pet.Cat;
-import animal.pet.Cow;
-import animal.pet.Dog;
-import animal.predator.Lion;
-import animal.predator.Shark;
-import animal.predator.Wolf;
-import animal.service.AnimalsRepositoryImpl;
-import animal.service.CreateAnimalService;
 import animal.service.CreateAnimalServiceImpl;
-import animal.service.HelperService;
-import custexceptions.InvalidAnimalBirthDateException;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Главный класс с вызовом метода main
@@ -24,6 +6,7 @@ import java.util.Map;
 public class Main {
     /**
      * Точка старта приложения
+     *
      * @param args - входящие параметры
      */
     public static void main(String[] args) {
@@ -39,23 +22,24 @@ public class Main {
         System.out.println("\nDo-While cycle:");
         createAnimalServiceImpl.createAnimals();*/
 
-        System.out.println("\nFor cycle:");
-        Map<String, List<Animal>> cycleAnimals = createAnimalServiceImpl.createAnimals(10);
+//        System.out.println("\nFor cycle:");
+//        Map<String, List<Animal>> cycleAnimals = createAnimalServiceImpl.createAnimals(10);
+//
+//        for (String name: cycleAnimals.keySet()) {
+//            String key = name.toString();
+//            String value = cycleAnimals.get(name).toString();
+//            System.out.println(key + " " + value);
+//        }
+//
+//        System.out.println("\nWhile cycle:");
+//        Map<String, List<Animal>> whileAnimals = createAnimalServiceImpl.createAnimalsfromInterface();
+//
+//        for (String name: whileAnimals.keySet()) {
+//            String key = name.toString();
+//            String value = whileAnimals.get(name).toString();
+//            System.out.println(key + " " + value);
+//        }
 
-        for (String name: cycleAnimals.keySet()) {
-            String key = name.toString();
-            String value = cycleAnimals.get(name).toString();
-            System.out.println(key + " " + value);
-        }
-
-        System.out.println("\nWhile cycle:");
-        Map<String, List<Animal>> whileAnimals = createAnimalServiceImpl.createAnimalsfromInterface();
-
-        for (String name: whileAnimals.keySet()) {
-            String key = name.toString();
-            String value = whileAnimals.get(name).toString();
-            System.out.println(key + " " + value);
-        }
     }
 
 }
--- HW-1/src/main/java/animal/service/AnimalsRepositoryImpl.java
+++ HW-1/src/main/java/animal/service/AnimalsRepositoryImpl.java
@@ -3,16 +3,37 @@ package animal.service;
 import animal.Animal;
 import custexceptions.EmptyAnimalArrayException;
 import custexceptions.NullAnimalArrayException;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Класс-имплементация интерфейса AnimalsRepository
  */
 public class AnimalsRepositoryImpl implements AnimalsRepository {
+//    @Override
+//    public Map<String, LocalDate> findLeapYearNames(Animal[] animals) {
+//        if (animals == null) {
+//            throw new NullAnimalArrayException();
+//        }
+//        if (animals.length == 0) {
+//            throw new EmptyAnimalArrayException();
+//        }
+//        Map<String, LocalDate> map = new HashMap<>();
+//        for (Animal a : animals) {
+//            if (a.getBirthDate().isLeapYear()) {
+//                map.put(a.getClass().getSimpleName() + " " + a.getName(), a.getBirthDate());
+//            }
+//        }
+//        return map;
+//    }
+
+
     @Override
     public Map<String, LocalDate> findLeapYearNames(Animal[] animals) {
         if (animals == null) {
@@ -21,14 +42,50 @@ public class AnimalsRepositoryImpl implements AnimalsRepository {
         if (animals.length == 0) {
             throw new EmptyAnimalArrayException();
         }
-        Map<String, LocalDate> map = new HashMap<>();
-        for (Animal a : animals) {
-            if (a.getBirthDate().isLeapYear()) {
-                map.put(a.getClass().getSimpleName() + " " + a.getName(), a.getBirthDate());
-            }
-        }
+
+        Map<String, LocalDate> map = Arrays.stream(animals)
+                .filter(a -> a.getBirthDate().isLeapYear())
+                .collect(Collectors.toMap(a -> a.getClass().getSimpleName() + " " + a.getName(), Animal::getBirthDate));
+
         return map;
     }
+
+//    @Override
+//    public Map<Animal, Integer> findOlderAnimal(Animal[] animals, int n) {
+//        if (animals == null) {
+//            throw new NullAnimalArrayException();
+//        }
+//        if (animals.length == 0) {
+//            throw new EmptyAnimalArrayException();
+//        }
+//
+//        Map<Animal, Integer> map = new HashMap<>();
+//        int maxAge = 0;
+//
+//        for (Animal a : animals) {
+//            int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
+//
+//            if (age > maxAge) {
+//                maxAge = age;
+//            }
+//
+//            if (age > n) {
+//                map.put(a, age);
+//            }
+//        }
+//        //Если не нашлось животных возрастом больше n
+//        if (map.isEmpty()) {
+//            //Находим всех животных из массива с максимальный возрастом
+//            for (Animal a : animals) {
+//                int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
+//                if (age == maxAge) {
+//                    map.put(a, age);
+//                }
+//            }
+//        }
+//        return map;
+//    }
+
 
     @Override
     public Map<Animal, Integer> findOlderAnimal(Animal[] animals, int n) {
@@ -39,35 +96,45 @@ public class AnimalsRepositoryImpl implements AnimalsRepository {
             throw new EmptyAnimalArrayException();
         }
 
-        Map<Animal, Integer> map = new HashMap<>();
-        int maxAge = 0;
+        Map<Animal, Integer> result = Arrays.stream(animals)
+                .filter(a -> {
+                    int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
+                    return age > n;
+                }).collect(Collectors.toMap(a -> a, a -> Period.between(a.getBirthDate(), LocalDate.now()).getYears()));
 
-        for (Animal a : animals) {
-            int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
-
-            if (age > maxAge) {
-                maxAge = age;
-            }
-
-            if (age > n) {
-                map.put(a, age);
-            }
+        if (result.isEmpty()) {
+            OptionalInt maxAge = Arrays.stream(animals).mapToInt(a -> Period.between(a.getBirthDate(), LocalDate.now()).getYears()).max();
+            result = Arrays.stream(animals)
+                    .filter(a -> Period.between(a.getBirthDate(), LocalDate.now()).getYears() == maxAge.getAsInt())
+                    .collect(Collectors.toMap(
+                            a -> a,
+                            a -> maxAge.getAsInt()
+                    ));
         }
-        //Если не нашлось животных возрастом больше n
-        if (map.isEmpty()) {
-            //Находим всех животных из массива с максимальный возрастом
-            for (Animal a : animals) {
-                int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
-                if (age == maxAge) {
-                    map.put(a, age);
-                }
-            }
-        }
-        return map;
+
+        return result;
     }
 
+//    @Override
+//    public Map<String, Integer> findDuplicate(Animal[] animals) {
+//        if (animals == null) {
+//            throw new NullAnimalArrayException();
+//        }
+//        if (animals.length == 0) {
+//            throw new EmptyAnimalArrayException();
+//        }
+//
+//        Map<String, Integer> map = new HashMap<>();
+//        for (Animal a : animals) {
+//            String type = a.getClass().getSimpleName();
+//            map.merge(type, 1, Integer::sum);
+//        }
+//        return map;
+//    }
+
+
     @Override
-    public Map<String, Integer> findDuplicate(Animal[] animals) {
+    public Map<String, List<Animal>> findDuplicate(Animal[] animals) {
         if (animals == null) {
             throw new NullAnimalArrayException();
         }
@@ -75,15 +142,62 @@ public class AnimalsRepositoryImpl implements AnimalsRepository {
             throw new EmptyAnimalArrayException();
         }
 
-        Map<String, Integer> map = new HashMap<>();
-        for (Animal a : animals) {
-            String type = a.getClass().getSimpleName();
-            if (map.get(type) == null) {
-                map.put(type, 1);
-            } else {
-                map.put(type, map.get(type) + 1);
-            }
-        }
+        Map<String, List<Animal>> map = Arrays.stream(animals)
+                .collect(Collectors.groupingBy(a -> a.getClass().getSimpleName(), Collectors.mapping(a -> a, toList())));
+
         return map;
+    }
+
+    @Override
+    public double findAverageAge(@NotNull List<Animal> animals) {
+        if (animals.isEmpty()) {
+            throw new EmptyAnimalArrayException("На вход передан пустой список");
+        }
+        double avgAge = animals.stream()
+                .mapToInt(a -> {
+                    int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
+                    return age;
+                })
+                .average()
+                .orElse(0);
+
+        return Math.round(avgAge * 100.0) / 100.0;
+    }
+
+    @Override
+    public List<Animal> findOldAndExpensive(@NotNull List<Animal> animals) {
+        if (animals.isEmpty()) {
+            throw new EmptyAnimalArrayException("На вход передан пустой список");
+        }
+        double avgCost = animals.stream()
+                .mapToDouble(Animal::getCost)
+                .average()
+                .orElse(0);
+
+        List<Animal> list = animals.stream()
+                .filter(a -> {
+                    int age = Period.between(a.getBirthDate(), LocalDate.now()).getYears();
+                    //Сделал больше 18 (не 5 как в задании), т.к. животные для теста раскиданы в этом диапозоне
+                    return age > 18 && a.getCost() > avgCost;
+                })
+                .sorted((Comparator.comparing(Animal::getBirthDate))).collect(Collectors.toList());
+
+        return list;
+    }
+
+    @Override
+    public List<Animal> findMinConstAnimals(@NotNull List<Animal> animals) {
+        if (animals.isEmpty()) {
+            throw new EmptyAnimalArrayException("На вход передан пустой список");
+        }
+        List<Animal> list = animals.stream()
+                .sorted(Comparator.comparing(Animal::getCost))
+                .collect(Collectors.toList())
+                .stream()
+                .limit(3)
+                .sorted(((o1, o2) -> -o1.getName().compareTo(o2.getName())))
+                .collect(Collectors.toList());
+
+        return list;
     }
 }
