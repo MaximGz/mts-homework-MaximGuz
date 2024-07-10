cbecb53f4dd8a95432cc7cfc2566628d0a1e651b
username <maks.guz.98@bk.ru>
2024-07-10 14:20:41 +0300

    HW-18

--- HW-18/src/main/java/org/example/hw18/config/UserConfig.java
+++ HW-18/src/main/java/org/example/hw18/config/UserConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -22,36 +23,36 @@ public class UserConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService user(PasswordEncoder passwordEncoder) {
-        System.out.println("Creating users during application startup...");
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN").build();
-        UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user123")).roles("USER").build();
-        UserDetails max = User.builder().username("max").password(passwordEncoder.encode("maxim")).roles("ADMIN", "USER").build();
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT username, password, enabled FROM db_security.users WHERE username = ?");
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT username, authority FROM db_security.authorities WHERE username = ?");
-        jdbcUserDetailsManager.setCreateUserSql("INSERT INTO db_security.users (username, password, enabled) values (?,?,?)");
-        jdbcUserDetailsManager.setCreateAuthoritySql("INSERT INTO db_security.authorities (username, authority) values (?,?)");
-        jdbcUserDetailsManager.setDeleteUserAuthoritiesSql("DELETE FROM db_security.authorities WHERE username = ?");
-        jdbcUserDetailsManager.setDeleteUserSql("DELETE FROM db_security.users WHERE username = ?");
-        jdbcUserDetailsManager.setUpdateUserSql("UPDATE db_security.users SET password = ?, enabled = ? WHERE username = ?");
-        jdbcUserDetailsManager.setChangePasswordSql("UPDATE db_security.users SET password = ? WHERE username = ?");
-        jdbcUserDetailsManager.createUser(admin);
-        jdbcUserDetailsManager.createUser(user);
-        jdbcUserDetailsManager.createUser(max);
-
-        System.out.println("Users created successfully.");
-        return jdbcUserDetailsManager;
-    }
-
-//    //Конфигурация In-Memory аутентификации:
 //    @Bean
-//    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+//    public UserDetailsService user(PasswordEncoder passwordEncoder) {
+//        System.out.println("Creating users during application startup...");
 //        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN").build();
 //        UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user123")).roles("USER").build();
 //        UserDetails max = User.builder().username("max").password(passwordEncoder.encode("maxim")).roles("ADMIN", "USER").build();
-//        return new InMemoryUserDetailsManager(admin, user, max);
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//
+//        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT username, password, enabled FROM db_security.users WHERE username = ?");
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT username, authority FROM db_security.authorities WHERE username = ?");
+//        jdbcUserDetailsManager.setCreateUserSql("INSERT INTO db_security.users (username, password, enabled) values (?,?,?)");
+//        jdbcUserDetailsManager.setCreateAuthoritySql("INSERT INTO db_security.authorities (username, authority) values (?,?)");
+//        jdbcUserDetailsManager.setDeleteUserAuthoritiesSql("DELETE FROM db_security.authorities WHERE username = ?");
+//        jdbcUserDetailsManager.setDeleteUserSql("DELETE FROM db_security.users WHERE username = ?");
+//        jdbcUserDetailsManager.setUpdateUserSql("UPDATE db_security.users SET password = ?, enabled = ? WHERE username = ?");
+//        jdbcUserDetailsManager.setChangePasswordSql("UPDATE db_security.users SET password = ? WHERE username = ?");
+//        jdbcUserDetailsManager.createUser(admin);
+//        jdbcUserDetailsManager.createUser(user);
+//        jdbcUserDetailsManager.createUser(max);
+//
+//        System.out.println("Users created successfully.");
+//        return jdbcUserDetailsManager;
 //    }
+
+    //Конфигурация In-Memory аутентификации:
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN").build();
+        UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user123")).roles("USER").build();
+        UserDetails max = User.builder().username("max").password(passwordEncoder.encode("maxim")).roles("ADMIN", "USER").build();
+        return new InMemoryUserDetailsManager(admin, user, max);
+    }
 }
