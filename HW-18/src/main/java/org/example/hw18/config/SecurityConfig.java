abaa019ee61102413a3d64f4ed85f1d3adbf25d5
username <maks.guz.98@bk.ru>
2024-07-10 14:14:49 +0300

    HW-18

--- HW-18/src/main/java/org/example/hw18/config/SecurityConfig.java
+++ HW-18/src/main/java/org/example/hw18/config/SecurityConfig.java
@@ -3,71 +3,53 @@ package org.example.hw18.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
     @Autowired
-    private DataSource dataSource;
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
-    public UserDetailsService user(PasswordEncoder passwordEncoder) {
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN").build();
-        UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user123")).roles("USER").build();
-        UserDetails max = User.builder().username("max").password(passwordEncoder.encode("maxim")).roles("ADMIN", "USER").build();
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT username, password, enabled FROM db_security.users WHERE username = ?");
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT username, authority FROM db_security.authorities WHERE username = ?");
-        jdbcUserDetailsManager.setCreateUserSql("INSERT INTO db_security.users (username, password, enabled) values (?,?,?)");
-        jdbcUserDetailsManager.setCreateAuthoritySql("INSERT INTO db_security.authorities (username, authority) values (?,?)");
-        jdbcUserDetailsManager.setDeleteUserAuthoritiesSql("DELETE FROM db_security.authorities WHERE username = ?");
-        jdbcUserDetailsManager.setDeleteUserSql("DELETE FROM db_security.users WHERE username = ?");
-        jdbcUserDetailsManager.setUpdateUserSql("UPDATE db_security.users SET password = ?, enabled = ? WHERE username = ?");
-        jdbcUserDetailsManager.setChangePasswordSql("UPDATE db_security.users SET password = ? WHERE username = ?");
-        jdbcUserDetailsManager.createUser(admin);
-        jdbcUserDetailsManager.createUser(user);
-        jdbcUserDetailsManager.createUser(max);
-        return jdbcUserDetailsManager;
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
     }
 
-    //Конфигурация In-Memory аутентификации:
-//    @Bean
-//    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-//        UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN").build();
-//        UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user123")).roles("USER").build();
-//        UserDetails max = User.builder().username("max").password(passwordEncoder.encode("maxim")).roles("ADMIN", "USER").build();
-//        return new InMemoryUserDetailsManager(admin, user, max);
-//    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate").permitAll()
                         .requestMatchers("/welcome").permitAll()
                         .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(form -> form
                         .loginPage("/login")
+                        //.loginPage("/logtest")
                         .defaultSuccessUrl("/home", true)
                         .permitAll()
                         //.failureHandler(customAuthenticationFailureHandler)
@@ -80,7 +62,7 @@ public class SecurityConfig {
                         .key("my_secret_key")
                         .tokenValiditySeconds(120) //
                         .rememberMeParameter("remember-me")
-                        .userDetailsService(user(passwordEncoder())))
+                        .userDetailsService(userDetailsService))
                 .build();
     }
 }
--- HW-18/src/main/java/org/example/hw18/controller/CustomerController.java
+++ HW-18/src/main/java/org/example/hw18/controller/CustomerController.java
@@ -6,7 +6,9 @@ import org.example.hw18.service.CustomerService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
